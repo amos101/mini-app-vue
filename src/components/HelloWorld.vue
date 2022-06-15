b85a8d426a936bf12e1fb954641e9f7641b99c91
Buso Amos <64058677+amos101@users.noreply.github.com>
2022-06-15 13:08:41 +0900

    mini-app-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div @click="btnChange()" id="btn">ルーレット</div>
+    <div @click="btnChange()" id="btn">おみくじルーレット</div>
+    <div @click="btnChangeTwo()" id="btnTwo">おみくじルーレット</div>
   </div>
 </template>
 
@@ -12,10 +13,26 @@ export default {
     msg: String,
   },
   methods: {
+    //配列の中身をrandomに行うやり方
     btnChange() {
-      const btn = document.getElementById("btn");
-      btn.textContent = "hit";
+      const results = ["大吉", "中吉", "小吉", "末吉"]
+      const btn = document.getElementById("btn")
+      const n = Math.floor(Math.random() * results.length)
+      btn.textContent = results[n]
     },
+
+    //確率を与えたい場合
+    btnChangeTwo() {
+      const btnTwo = document.getElementById("btnTwo")
+      const n = Math.random();
+      if (n < 0.05) {
+        btnTwo.textContent = '大吉'; // 5%
+      } else if (n < 0.2) {
+        btnTwo.textContent = '中吉'; // 15%
+      } else {
+        btnTwo.textContent = '凶'; // 80%
+      }
+    }
   },
 };
 </script>
@@ -23,16 +40,16 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #btn {
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background: #ef454a;
   border-radius: 50%;
   margin: 30px auto;
   text-align: center;
-  line-height: 200px;
+  line-height: 100px;
   color: #fff;
   font-weight: bold;
-  font-size: 22px;
+  font-size: 10px;
   cursor: pointer;
   box-shadow: 0 10px 0 #d1483e;
   user-select: none;
@@ -43,6 +60,34 @@ export default {
 }
 
 #btn:active {
+  box-shadow: 0 5px 0 #d1483e;
+  margin-top: 35px;
+}
+
+h1 {
+  font-size: 10px;
+}
+#btnTwo {
+  width: 100px;
+  height: 100px;
+  background: #ef454a;
+  border-radius: 50%;
+  margin: 30px auto;
+  text-align: center;
+  line-height: 100px;
+  color: #fff;
+  font-weight: bold;
+  font-size: 10px;
+  cursor: pointer;
+  box-shadow: 0 10px 0 #d1483e;
+  user-select: none;
+}
+
+#btnTwo:hover {
+  opacity: 0.9;
+}
+
+#btnTwo:active {
   box-shadow: 0 5px 0 #d1483e;
   margin-top: 35px;
 }
